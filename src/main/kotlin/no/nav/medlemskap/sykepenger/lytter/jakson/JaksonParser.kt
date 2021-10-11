fc6f3cf6c3312cd55b2eb03604b86a2a5def5d60
hans-jacob Melby <hans-jacob.melby@nav.no>
2021-10-12 01:32:33 +0200

    dropping messages when parse error

--- src/main/kotlin/no/nav/medlemskap/sykepenger/lytter/jakson/JaksonParser.kt
+++ src/main/kotlin/no/nav/medlemskap/sykepenger/lytter/jakson/JaksonParser.kt
@@ -3,9 +3,17 @@ package no.nav.medlemskap.sykepenger.lytter.jakson
 import com.fasterxml.jackson.databind.*
 import com.fasterxml.jackson.module.kotlin.readValue
 import com.fasterxml.jackson.module.kotlin.registerKotlinModule
+import mu.KotlinLogging
 import no.nav.medlemskap.sykepenger.lytter.domain.LovmeSoknadDTO
+import no.nav.medlemskap.sykepenger.lytter.domain.SoknadsstatusDTO
+import no.nav.medlemskap.sykepenger.lytter.domain.SoknadstypeDTO
+import no.nav.medlemskap.sykepenger.lytter.service.LovMeService
+import java.time.LocalDate
+import java.time.LocalDateTime
+
 
 class JaksonParser {
+    private val log = KotlinLogging.logger { }
     fun parse(jsonString: String): LovmeSoknadDTO {
         try {
             val mapper: ObjectMapper = ObjectMapper()
@@ -18,8 +26,14 @@ class JaksonParser {
             return  mapper.readValue(jsonString)
         }
         catch (t:Throwable){
-            throw t
-            //TODO: hvordan skal/bør vi håndtere dette...
+            log.error("Unable to parse json. Dropping message. Cause : ${t.message}")
+            return LovmeSoknadDTO("",
+                SoknadstypeDTO.ARBEIDSTAKERE,SoknadsstatusDTO.SENDT,
+                "",null,
+                LocalDate.now(),
+                LocalDateTime.now(),
+                LocalDate.now(),
+                LocalDate.now(),false)
         }
     }
 
