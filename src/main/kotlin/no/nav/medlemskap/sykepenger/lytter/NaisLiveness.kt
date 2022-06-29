0adf2f5a3e8b044c84a39c890175f33da3fd64b9
hans-jacob Melby <hans-jacob.melby@nav.no>
2022-06-29 12:26:58 +0200

    forbedret status oversikt

--- src/main/kotlin/no/nav/medlemskap/sykepenger/lytter/NaisLiveness.kt
+++ src/main/kotlin/no/nav/medlemskap/sykepenger/lytter/NaisLiveness.kt
@@ -7,6 +7,7 @@ import io.ktor.response.*
 import io.ktor.routing.*
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
+import io.ktor.util.*
 import io.micrometer.prometheus.PrometheusMeterRegistry
 import io.prometheus.client.exporter.common.TextFormat
 import kotlinx.coroutines.Dispatchers
@@ -15,6 +16,7 @@ import kotlinx.coroutines.withContext
 import no.nav.medlemskap.sykepenger.lytter.service.BomloService
 import java.io.Writer
 import java.util.*
+import kotlin.collections.HashMap
 
 fun naisLiveness(medlemskalConsumerJob:Job,bomloService: BomloService) = embeddedServer(Netty, applicationEngineEnvironment {
     connector { port = 8080 }
@@ -36,7 +38,17 @@ fun naisLiveness(medlemskalConsumerJob:Job,bomloService: BomloService) = embedde
                 call.respondText("Ready!", ContentType.Text.Plain, HttpStatusCode.OK)
             }
             get("/status") {
-                call.respondText(bomloService.sagaClient.ping(UUID.randomUUID().toString()), ContentType.Text.Plain, HttpStatusCode.OK)
+                var map:MutableMap<String,Boolean> = mutableMapOf()
+                try{
+                    bomloService.sagaClient.ping(UUID.randomUUID().toString())
+                    map["SAGA"] = true
+                    call.respondText(Dependencies(map,null).toString(), ContentType.Text.Plain, HttpStatusCode.OK)
+                }
+                catch (t:Exception){
+                    map["SAGA"] = false
+                    call.respondText(Dependencies(map,t.message).toString(), ContentType.Text.Plain, HttpStatusCode.ExpectationFailed)
+                }
+
             }
             get("/metrics") {
                 call.respondTextWriter(ContentType.parse(TextFormat.CONTENT_TYPE_004)) {
@@ -55,3 +67,4 @@ suspend fun writeMetrics004(writer: Writer, registry: PrometheusMeterRegistry) {
     }
 }
 
+data class Dependencies(val hashMap: MutableMap<String,Boolean>, val message:String?)
