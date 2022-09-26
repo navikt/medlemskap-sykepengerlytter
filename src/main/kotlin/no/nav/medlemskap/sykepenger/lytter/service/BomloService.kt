59ccbe4c8cb5d97fe789ea378f4ecf0e2b40d9d7
hans-jacob Melby <hans-jacob.melby@nav.no>
2022-09-26 17:31:49 +0200

    slår opp mot persistenceService for å hente ut arbeidUtland flagg når kall mot Lovme skal skje for nytt endepunkt

--- src/main/kotlin/no/nav/medlemskap/sykepenger/lytter/service/BomloService.kt
+++ src/main/kotlin/no/nav/medlemskap/sykepenger/lytter/service/BomloService.kt
@@ -3,20 +3,32 @@ package no.nav.medlemskap.sykepenger.lytter.service
 import com.fasterxml.jackson.databind.JsonNode
 import io.ktor.client.features.*
 import mu.KotlinLogging
+import net.logstash.logback.argument.StructuredArguments
+import no.nav.medlemskap.saga.persistence.Brukersporsmaal
 import no.nav.medlemskap.sykepenger.lytter.clients.RestClients
 import no.nav.medlemskap.sykepenger.lytter.clients.azuread.AzureAdClient
 import no.nav.medlemskap.sykepenger.lytter.clients.medloppslag.*
 import no.nav.medlemskap.sykepenger.lytter.config.Configuration
+import no.nav.medlemskap.sykepenger.lytter.config.Environment
 import no.nav.medlemskap.sykepenger.lytter.config.objectMapper
+import no.nav.medlemskap.sykepenger.lytter.domain.LovmeSoknadDTO
 import no.nav.medlemskap.sykepenger.lytter.jackson.JacksonParser
+import no.nav.medlemskap.sykepenger.lytter.persistence.DataSourceBuilder
+import no.nav.medlemskap.sykepenger.lytter.persistence.PostgresBrukersporsmaalRepository
+import no.nav.medlemskap.sykepenger.lytter.persistence.PostgresMedlemskapVurdertRepository
 import no.nav.medlemskap.sykepenger.lytter.rest.BomloRequest
+import no.nav.medlemskap.sykepenger.lytter.security.sha256
+import java.time.LocalDate
 
 class BomloService(private val configuration: Configuration) {
     companion object {
         private val log = KotlinLogging.logger { }
 
     }
-
+    val persistenceService: PersistenceService = PersistenceService(
+        PostgresMedlemskapVurdertRepository(DataSourceBuilder(System.getenv()).getDataSource()) ,
+        PostgresBrukersporsmaalRepository(DataSourceBuilder(System.getenv()).getDataSource())
+    )
     val azureAdClient = AzureAdClient(configuration)
     val restClients = RestClients(
         azureAdClient = azureAdClient,
@@ -40,7 +52,8 @@ class BomloService(private val configuration: Configuration) {
         catch (cause: ResponseException){
             if (cause.response.status.value == 404) {
                 log.warn("ingen vurdering funnet. Kaller Lovme $callId", cause)
-                val lovmeRequest = mapToMedlemskapRequest(bomloRequest)
+                val arbeidUtland = getArbeidUtlandFromBrukerSporsmaal(bomloRequest, callId)
+                val lovmeRequest = mapToMedlemskapRequest(bomloRequest,arbeidUtland)
                 val resultat= lovmeClient.vurderMedlemskapBomlo(lovmeRequest,callId)
                 return JacksonParser().ToJson(resultat)
             }
@@ -49,9 +62,39 @@ class BomloService(private val configuration: Configuration) {
             throw cause
         }
     }
+    fun getArbeidUtlandFromBrukerSporsmaal(bomloRequest: BomloRequest,callId: String): Boolean {
 
-    private fun mapToMedlemskapRequest(bomloRequest: BomloRequest): MedlOppslagRequest {
-        return MedlOppslagRequest(bomloRequest.fnr,bomloRequest.førsteDagForYtelse.toString(), Periode(bomloRequest.periode.fom.toString(),bomloRequest.periode.tom.toString()), Brukerinput(true))
+        val brukersporsmaal = persistenceService.hentbrukersporsmaalForFnr(bomloRequest.fnr).filter { it.eventDate.isAfter(
+            LocalDate.now().minusYears(1)) }
+        val jasvar =  brukersporsmaal.filter { it.sporsmaal?.arbeidUtland ==true }
+        val neisvar =  brukersporsmaal.filter { it.sporsmaal?.arbeidUtland ==false }
+        val ikkeoppgittsvar = brukersporsmaal.filter { it.sporsmaal?.arbeidUtland ==null }
+        //krav 2 : Er det svart JA på tidligere spørsmål, bruk denne verdien
+        if (jasvar.isNotEmpty()) {
+            log.info("arbeid utland ja oppgitt i tidligere søknader siste året (${jasvar.first().soknadid}) for fnr (kryptert) ${bomloRequest.fnr.sha256()}. Setter arbeid utland lik true",
+                StructuredArguments.kv("callId", callId)
+            )
+            return true
+        }
+        //krav 3 : er det svart NEI på tidligere søknader så bruk denne verdien
+        if (neisvar.isNotEmpty()){
+            log.info("arbeid utland Nei oppgitt i tidligere søknader siste året (${neisvar.first().soknadid}) for fnr (kryptert) ${bomloRequest.fnr.sha256()}. Setter arbeid utland lik false",
+                StructuredArguments.kv("callId", callId)
+            )
+            return false
+        }
+        if (ikkeoppgittsvar.isEmpty()){
+            log.info("arbeid utland er ikke oppgitt  i søknad ${callId}, og heller aldri oppgitt i tidligere søknader siste året for fnr (kryptert) ${bomloRequest.fnr.sha256()}. Setter arbeid utland lik true")
+            return true
+        }
+
+        else{
+            return false
+        }
+    }
+
+    private fun mapToMedlemskapRequest(bomloRequest: BomloRequest,arbeidUtland:Boolean): MedlOppslagRequest {
+        return MedlOppslagRequest(bomloRequest.fnr,bomloRequest.førsteDagForYtelse.toString(), Periode(bomloRequest.periode.fom.toString(),bomloRequest.periode.tom.toString()), Brukerinput(arbeidUtland))
 
     }
 }
