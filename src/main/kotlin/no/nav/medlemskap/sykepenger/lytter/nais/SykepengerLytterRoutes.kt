7afa68bf51f60fe80a5b5e6e77cd6c0886105b44
Stian S. Douzette <stians.douzette@nav.no>
2023-01-27 13:19:44 +0100

    Logger medlemskapsresultater til securelogs

--- src/main/kotlin/no/nav/medlemskap/sykepenger/lytter/nais/SykepengerLytterRoutes.kt
+++ src/main/kotlin/no/nav/medlemskap/sykepenger/lytter/nais/SykepengerLytterRoutes.kt
@@ -1,5 +1,6 @@
 package no.nav.medlemskap.sykepenger.lytter.nais
 
+import com.fasterxml.jackson.databind.JsonNode
 import io.ktor.server.application.*
 import io.ktor.server.auth.*
 import io.ktor.server.auth.jwt.*
@@ -10,7 +11,8 @@ import io.ktor.server.routing.*
 import io.ktor.http.*
 import mu.KotlinLogging
 import net.logstash.logback.argument.StructuredArguments.kv
-import no.nav.medlemskap.sykepenger.lytter.clients.medloppslag.MedlOppslagRequest
+import no.nav.medlemskap.sykepenger.lytter.config.objectMapper
+import no.nav.medlemskap.sykepenger.lytter.jackson.JacksonParser
 import no.nav.medlemskap.sykepenger.lytter.rest.BomloRequest
 import no.nav.medlemskap.sykepenger.lytter.service.BomloService
 import java.util.*
@@ -18,29 +20,53 @@ import java.util.*
 private val logger = KotlinLogging.logger { }
 private val secureLogger = KotlinLogging.logger("tjenestekall")
 fun Routing.sykepengerLytterRoutes(bomloService: BomloService) {
-        authenticate("azureAuth") {
-            post("/vurdering") {
-                val callerPrincipal: JWTPrincipal = call.authentication.principal()!!
-                val azp = callerPrincipal.payload.getClaim("azp").asString()
-                secureLogger.info("EvalueringRoute: azp-claim i principal-token: {} ", azp)
-                val callId = call.callId ?: UUID.randomUUID().toString()
-                logger.info(
-                    "kall autentisert, url : /vurdering",
-                    kv("callId", callId)
-                )
-                val request = call.receive<BomloRequest>()
-                try {
-                    val response = bomloService.finnVurdering(request,callId)
-                    call.respond(HttpStatusCode.OK, response)
-                } catch (t: Throwable) {
-                    secureLogger.error("Unexpected error calling Lovmme",
-                    kv("callId", callId),
-                    kv("fnr",request.fnr),
-                    kv("cause",t.stackTrace)
-                    )
-                    call.respond(HttpStatusCode.InternalServerError,t.message!!)
-                }
-            }
+    authenticate("azureAuth") {
+        post("/vurdering") {
+            val callerPrincipal: JWTPrincipal = call.authentication.principal()!!
+            val azp = callerPrincipal.payload.getClaim("azp").asString()
+            secureLogger.info("EvalueringRoute: azp-claim i principal-token: {} ", azp)
+            val callId = call.callId ?: UUID.randomUUID().toString()
+            logger.info(
+                "kall autentisert, url : /vurdering",
+                kv("callId", callId)
+            )
+            val request = call.receive<BomloRequest>()
+            try {
+                val response = bomloService.finnVurdering(request, callId)
+                val medlemskapsloggObjekt = response.hentMedlemskapsResultat()
 
+                secureLogger.info(
+                    "{} konklusjon gitt for bruker {}", medlemskapsloggObjekt.svar, medlemskapsloggObjekt.fnr,
+                    kv("fnr", medlemskapsloggObjekt.fnr),
+                    kv("svar", medlemskapsloggObjekt.svar),
+                    kv("årsak", medlemskapsloggObjekt.årsak),
+                    kv("årsaker", medlemskapsloggObjekt.årsaker)
+                )
+
+                call.respond(HttpStatusCode.OK, response)
+            } catch (t: Throwable) {
+                secureLogger.error(
+                    "Unexpected error calling Lovmme",
+                    kv("callId", callId),
+                    kv("fnr", request.fnr),
+                    kv("cause", t.stackTrace)
+                )
+                call.respond(HttpStatusCode.InternalServerError, t.message!!)
+            }
         }
+
+    }
 }
+
+fun JsonNode.hentMedlemskapsResultat(): Medlemskapslogg {
+    val årsaker = this.get("resultat").get("årsaker")
+
+    return Medlemskapslogg(
+        fnr = this.get("datagrunnlag").get("fnr").asText(),
+        svar = this.get("resultat").get("svar").asText(),
+        årsak = årsaker.firstOrNull()?.get("regelId"),
+        årsaker = årsaker.map { it.get("regelId") }
+    )
+}
+
+data class Medlemskapslogg(val fnr: String, val svar: String, val årsak: JsonNode?, val årsaker: List<JsonNode>)
