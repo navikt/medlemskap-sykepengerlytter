41d2696601451efd26f1480fbe00a873ac1f61a3
hans-jacob Melby <hans-jacob.melby@nav.no>
2023-10-11 12:14:16 +0200

    endrer pax_poll_record config til 1

--- src/main/kotlin/no/nav/medlemskap/sykepenger/lytter/config/KafkaConfig.kt
+++ src/main/kotlin/no/nav/medlemskap/sykepenger/lytter/config/KafkaConfig.kt
@@ -36,7 +36,7 @@ open class KafkaConfig(
         ConsumerConfig.GROUP_ID_CONFIG to Configuration.KafkaConfig().flexConsumerGroup,
         ConsumerConfig.AUTO_OFFSET_RESET_CONFIG to "earliest",
         ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG to "false",
-        ConsumerConfig.MAX_POLL_RECORDS_CONFIG to 100,
+        ConsumerConfig.MAX_POLL_RECORDS_CONFIG to 1,
 
         ) + securityStrategy.securityConfig()
 
