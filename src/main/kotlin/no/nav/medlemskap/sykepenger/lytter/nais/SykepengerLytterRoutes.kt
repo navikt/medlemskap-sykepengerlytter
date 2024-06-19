f0cd3add8c667ddade621fe6ca3d2d2916cec5ec
hans-jacob Melby <hans-jacob.melby@nav.no>
2024-06-19 20:32:38 +0200

    forbedrer logging slik at vi enklere kan hente ut rapporter om hvilke brukere speil og flex ber om status for

--- src/main/kotlin/no/nav/medlemskap/sykepenger/lytter/nais/SykepengerLytterRoutes.kt
+++ src/main/kotlin/no/nav/medlemskap/sykepenger/lytter/nais/SykepengerLytterRoutes.kt
@@ -79,6 +79,15 @@ fun Routing.sykepengerLytterRoutes(bomloService: BomloService) {
             try {
                 val response = bomloService.finnFlexVurdering(request, callId)
                 val speilRespons = response.lagSpeilRespons(callId)
+                val timeInMS = System.currentTimeMillis()-start
+                secureLogger.info("{} svar funnet for Speil for bruker {}", speilRespons.speilSvar.name, speilRespons.fnr,
+                    kv("callId", callId),
+                    kv("fnr", request.fnr),
+                    kv("tidsbrukInMs", timeInMS),
+                    kv("endpoint", "speilvurdering"),
+                    kv("soknadId", speilRespons.soknadId),
+                    kv("konklusjon", speilRespons.speilSvar.name),
+                )
 
                 call.respond(HttpStatusCode.OK, speilRespons)
             } catch (t: Throwable) {
@@ -111,7 +120,7 @@ fun Routing.sykepengerLytterRoutes(bomloService: BomloService) {
                     secureLogger.info(
                         "{} svar funnet for flex for bruker {}", response.status, response.fnr,
                         kv("fnr", response.fnr),
-                        kv("svar", response.status),
+                        kv("konklusjon", response.status),
                         kv("endpoint", "flexvurdering")
                     )
                     call.respond(HttpStatusCode.OK, response)
