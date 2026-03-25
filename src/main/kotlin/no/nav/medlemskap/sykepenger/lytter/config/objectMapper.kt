a071f99c672924038091a6743a11b61c5db6243a
Kieu Nguyen <kieunguyen@NO-FV9YQ6X7NL.local>
2026-03-25 12:20:43 +0100

    Rettet opp deprecated metoder for Jackson. Refaktorering og cleanup med fokus på gjenbruk.

--- src/main/kotlin/no/nav/medlemskap/sykepenger/lytter/config/objectMapper.kt
+++ src/main/kotlin/no/nav/medlemskap/sykepenger/lytter/config/objectMapper.kt
@@ -1,15 +1,17 @@
 package no.nav.medlemskap.sykepenger.lytter.config
 
 import com.fasterxml.jackson.databind.*
+import com.fasterxml.jackson.databind.json.JsonMapper
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
-import com.fasterxml.jackson.module.kotlin.registerKotlinModule
+import com.fasterxml.jackson.module.kotlin.KotlinModule
 
 
-val objectMapper: ObjectMapper = ObjectMapper()
-    .registerKotlinModule()
-    .registerModule(JavaTimeModule())
-    .configure(SerializationFeature.INDENT_OUTPUT, true)
-    .configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false)
-    .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
-    .configure(MapperFeature.ACCEPT_CASE_INSENSITIVE_ENUMS, true)
+val objectMapper: ObjectMapper = JsonMapper.builder()
+    .addModule(KotlinModule.Builder().build())
+    .addModule(JavaTimeModule())
+    .enable(SerializationFeature.INDENT_OUTPUT)
+    .disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
+    .disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES)
+    .enable(MapperFeature.ACCEPT_CASE_INSENSITIVE_ENUMS)
+    .build()
 
--- src/main/kotlin/no/nav/medlemskap/sykepenger/lytter/jackson/MedlemskapVurdertParser.kt
+++ src/main/kotlin/no/nav/medlemskap/sykepenger/lytter/jackson/MedlemskapVurdertParser.kt
@@ -1,32 +1,28 @@
 package no.nav.medlemskap.sykepenger.lytter.jackson
 
 import com.fasterxml.jackson.databind.*
+import com.fasterxml.jackson.databind.json.JsonMapper
+import com.fasterxml.jackson.module.kotlin.KotlinModule
 import com.fasterxml.jackson.module.kotlin.readValue
-import com.fasterxml.jackson.module.kotlin.registerKotlinModule
 
 class MedlemskapVurdertParser {
 
-    fun parse(jsonString: String): JsonNode {
-        val mapper: ObjectMapper = ObjectMapper()
-            .registerKotlinModule()
-            .findAndRegisterModules()
-            .configure(SerializationFeature.INDENT_OUTPUT, true)
-            .configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false)
-            .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
-            .configure(MapperFeature.ACCEPT_CASE_INSENSITIVE_ENUMS, true)
-        return mapper.readValue(jsonString)
+    companion object {
+        private val mapper: ObjectMapper = JsonMapper.builder()
+            .addModule(KotlinModule.Builder().build())
+            .findAndAddModules()
+            .enable(SerializationFeature.INDENT_OUTPUT)
+            .disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
+            .disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES)
+            .enable(MapperFeature.ACCEPT_CASE_INSENSITIVE_ENUMS)
+            .build()
+    }
 
+    fun parse(jsonString: String): JsonNode {
+        return mapper.readValue(jsonString)
     }
 
     fun ToJson(obj: Any): JsonNode {
-        val mapper: ObjectMapper = ObjectMapper()
-            .registerKotlinModule()
-            .findAndRegisterModules()
-            .configure(SerializationFeature.INDENT_OUTPUT, true)
-            .configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false)
-            .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
-            .configure(MapperFeature.ACCEPT_CASE_INSENSITIVE_ENUMS, true)
         return mapper.valueToTree(obj)
-
     }
 }
