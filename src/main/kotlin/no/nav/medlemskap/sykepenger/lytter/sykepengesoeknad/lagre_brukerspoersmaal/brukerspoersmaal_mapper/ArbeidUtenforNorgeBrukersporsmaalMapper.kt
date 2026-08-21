846cc62f62a8d3daa14e06c4bd8f6c0063ad2fb8
Stian.Douzette <stian.douzette@nav.no>
2026-08-21 14:46:39 +0200

    refaktorerer arbeid utenfor Norge brukerspørsmål mapping til å akseptere null verdier

--- src/main/kotlin/no/nav/medlemskap/sykepenger/lytter/sykepengesoeknad/lagre_brukerspoersmaal/brukerspoersmaal_mapper/ArbeidUtenforNorgeBrukersporsmaalMapper.kt
+++ src/main/kotlin/no/nav/medlemskap/sykepenger/lytter/sykepengesoeknad/lagre_brukerspoersmaal/brukerspoersmaal_mapper/ArbeidUtenforNorgeBrukersporsmaalMapper.kt
@@ -2,14 +2,17 @@ package no.nav.medlemskap.sykepenger.lytter.sykepengesoeknad.lagre_brukerspoersm
 
 import no.nav.medlemskap.sykepenger.lytter.persistence.ArbeidUtenforNorgeSpørsmål
 import no.nav.medlemskap.sykepenger.lytter.persistence.MedlemskapsBrukerSpørsmål
-import no.nav.medlemskap.sykepenger.lytter.sykepengesoeknad.lagre_brukerspoersmaal.brukerspoersmaal_mapper.BrukerSporsmaalMapperHjelper.erSvarPåBrukerspørsmålJa
 
 fun mapArbeidUtenforNorgeBrukerspørsmål(
     spørsmålListe: List<MedlemskapsBrukerSpørsmål>
 ): ArbeidUtenforNorgeSpørsmål {
-    val arbeidutland = spørsmålListe.find { it.tag == "ARBEID_UTENFOR_NORGE" }
-    var erSvarPåArbeidUtenforNorgeJa: Boolean? = null
-    if (arbeidutland?.svar != null)
-        erSvarPåArbeidUtenforNorgeJa = erSvarPåBrukerspørsmålJa(arbeidutland.svar)
+    val arbeidUtenforNorgeSvarVerdi = spørsmålListe
+        .find { it.tag == "ARBEID_UTENFOR_NORGE" }
+        ?.svar?.firstOrNull()?.verdi
+    val erSvarPåArbeidUtenforNorgeJa = when (arbeidUtenforNorgeSvarVerdi) {
+        "JA" -> true
+        "NEI" -> false
+        else -> null
+    }
     return ArbeidUtenforNorgeSpørsmål(erSvarPåArbeidUtenforNorgeJa)
 }
